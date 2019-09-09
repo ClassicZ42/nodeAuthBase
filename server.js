87103038221495d137adc0057005847236447582
Donovan <donovanvdl@Infoslips.com>
2019-09-09 15:31:41 +0200

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-var app = require('./app');
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 3000;
+var db = require('./config/db');
+// register user routes
+var UserController = require('./user/UserController');
+var AuthController = require('./auth/AuthController');
+
+app.use('/users', UserController);
+app.use('/api/auth', AuthController);
 
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
